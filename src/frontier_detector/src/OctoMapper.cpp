49f96b43871220a31a1a31a6379a20b8230be70c
WFram <earl.freedom.ea@gmail.com>
2023-03-27 04:18:19 +0300

    Fix look up transform issue

--- src/frontier_detector/src/OctoMapper.cpp
+++ src/frontier_detector/src/OctoMapper.cpp
@@ -14,13 +14,13 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "Octomapper");
     ros::start();
 
-    if(argc > 1) {
+    if (argc > 1) {
         ROS_WARN ("Arguments supplied via command line are neglected.");
     }
 
     ros::NodeHandle node_handle;
 
-    OctoMapper OCTO_NODE (node_handle);
+    OctoMapper OCTO_NODE(node_handle);
 
     OCTO_NODE.Init();
 
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
     while (ros::ok()) {
 
         // Update and publish OctoMap with extracted PC
-        OCTO_NODE.Update ();
+        OCTO_NODE.Update();
 
         // Spin ROS with rate r
         ros::spinOnce();
@@ -43,13 +43,13 @@ int main(int argc, char **argv) {
 
 }
 
-OctoMapper::OctoMapper (ros::NodeHandle &node_handle) {
+OctoMapper::OctoMapper(ros::NodeHandle &node_handle) {
 
     name_of_node_ = ros::this_node::getName();
     node_handle_ = node_handle;
     previous_cloud_size_ = 0;
     since_last_big_update_ = 0;
-    octo_origin_ = { 0.0, 0.0, 0.0 };
+    octo_origin_ = {0.0, 0.0, 0.0};
     octo_frame_ = octomap::pose6d(0, 0, 0, 0, 0, 0);
     update_octomap_ = false;
     clear_octomap_ = false;
@@ -60,7 +60,7 @@ OctoMapper::OctoMapper (ros::NodeHandle &node_handle) {
 }
 
 
-OctoMapper::~OctoMapper () {
+OctoMapper::~OctoMapper() {
 
     delete octomap_;
     delete pointcloud_map_points_;
@@ -68,7 +68,7 @@ OctoMapper::~OctoMapper () {
 }
 
 
-void OctoMapper::Init () {
+void OctoMapper::Init() {
 
     ROS_INFO("%s : Initializing.", name_of_node_.c_str());
 
@@ -79,34 +79,38 @@ void OctoMapper::Init () {
     tfBuffer.reset(new tf2_ros::Buffer);
     tfListener.reset(new tf2_ros::TransformListener(*tfBuffer));
 
+    usleep(5000000);
+
     // Enable publishing OctoMap
-    octomap_publisher_ = node_handle_.advertise<octomap_msgs::Octomap>(name_of_node_+"/octomap", 3);
+    octomap_publisher_ = node_handle_.advertise<octomap_msgs::Octomap>(name_of_node_ + "/octomap", 3);
 
     // PointCloud subscriber
-    map_PC_subscriber_ = node_handle_.subscribe<sensor_msgs::PointCloud2>("/map_points", 10, &OctoMapper::MapCallback, this);
-    ORBstate_subscriber_ = node_handle_.subscribe<orb_slam2_ros::ORBState>("/info/state", 10, &OctoMapper::ORBStateCallback, this);
-    GBAstate_subscriber_ = node_handle_.subscribe<std_msgs::Bool> ("/gba_running", 10, &OctoMapper::GBAStateCallback, this);
+    map_PC_subscriber_ = node_handle_.subscribe<sensor_msgs::PointCloud2>("/map_points", 10, &OctoMapper::MapCallback,
+                                                                          this);
+    ORBstate_subscriber_ = node_handle_.subscribe<orb_slam2_ros::ORBState>("/info/state", 10,
+                                                                           &OctoMapper::ORBStateCallback, this);
+    GBAstate_subscriber_ = node_handle_.subscribe<std_msgs::Bool>("/gba_running", 10, &OctoMapper::GBAStateCallback,
+                                                                  this);
 
     // Services
     //octoGetter_server_ = node_handle_.advertiseService(name_of_node_+"/get_octomap", &OctoMapper::GetOctomapSrv, this);
-
-    getTfTransformMatrix(T_optical_target_, "camera_link_optical", target_frame_id_param_);
+    getTfTransformMatrix(T_optical_target_, "zed2_left_camera_optical_frame", target_frame_id_param_);
 
     ROS_INFO("%s : Initialized successfully.", name_of_node_.c_str());
 
 }
 
-void OctoMapper::initializeParams(ros::NodeHandle &nh){
+void OctoMapper::initializeParams(ros::NodeHandle &nh) {
 
-    nh.param<int>(name_of_node_+ "/rate", rate_hz_, int(1));
+    nh.param<int>(name_of_node_ + "/rate", rate_hz_, int(1));
 
-    nh.param<bool>(name_of_node_+ "/octomap/publish_octomap", publish_octomap_param_, true);
+    nh.param<bool>(name_of_node_ + "/octomap/publish_octomap", publish_octomap_param_, true);
 
-    nh.param<std::string>(name_of_node_+ "/target_frame_id", target_frame_id_param_, "base_footprint");
-    nh.param<std::string>(name_of_node_+ "/pointcloud_frame_id", map_frame_id_param_, "map");
-    nh.param<double>(name_of_node_+ "/max_height", max_height_param_, 3.0);
+    nh.param<std::string>(name_of_node_ + "/target_frame_id", target_frame_id_param_, "base_footprint");
+    nh.param<std::string>(name_of_node_ + "/pointcloud_frame_id", map_frame_id_param_, "map");
+    nh.param<double>(name_of_node_ + "/max_height", max_height_param_, 3.0);
 
-    nh.param<bool>(name_of_node_+"/octomap/rebuild",octomap_rebuild_, false);
+    nh.param<bool>(name_of_node_ + "/octomap/rebuild", octomap_rebuild_, false);
 
 }
 
@@ -127,7 +131,7 @@ void OctoMapper::GBAStateCallback(const std_msgs::Bool::ConstPtr &msg) {
 }
 
 
-void OctoMapper::MapCallback (const sensor_msgs::PointCloud2::ConstPtr& msg) {
+void OctoMapper::MapCallback(const sensor_msgs::PointCloud2::ConstPtr &msg) {
     /* This callback consumes little time, so pull all map points every time
      */
 
@@ -143,7 +147,7 @@ void OctoMapper::MapCallback (const sensor_msgs::PointCloud2::ConstPtr& msg) {
         // Convert cloud from sensor_msgs::PointCloud to octomap::Pointcloud
         pointcloud_map_points_->clear();
         for (int i = 0; i < out_pointcloud.points.size(); i++) {
-            if(out_pointcloud.points[i].z < max_height_param_) {
+            if (out_pointcloud.points[i].z < max_height_param_) {
                 Eigen::Vector3d map_pt(out_pointcloud.points[i].x,
                                        out_pointcloud.points[i].y,
                                        out_pointcloud.points[i].z);
@@ -155,13 +159,14 @@ void OctoMapper::MapCallback (const sensor_msgs::PointCloud2::ConstPtr& msg) {
         update_octomap_ = true;
         ROS_DEBUG("%s: Pulled PC with %li points.", name_of_node_.c_str(), pointcloud_map_points_->size());
 
-    } else if (orb_state_ == 2){
+    } else if (orb_state_ == 2) {
         update_octomap_ = false;
-        ROS_WARN_THROTTLE(1,"%s: Not updating OctoMap if ORB-SLAM is LOST.", name_of_node_.c_str());
+        ROS_WARN_THROTTLE(1, "%s: Not updating OctoMap if ORB-SLAM is LOST.", name_of_node_.c_str());
     } else {
         update_octomap_ = false;
         clear_octomap_ = true;
-        ROS_WARN_THROTTLE(1,"%s: Not updating OctoMap if ORB-SLAM is UNKNOWN, NOT_READY, NOT_INITIALIZED.", name_of_node_.c_str());
+        ROS_WARN_THROTTLE(1, "%s: Not updating OctoMap if ORB-SLAM is UNKNOWN, NOT_READY, NOT_INITIALIZED.",
+                          name_of_node_.c_str());
     }
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
@@ -170,7 +175,7 @@ void OctoMapper::MapCallback (const sensor_msgs::PointCloud2::ConstPtr& msg) {
 }
 
 
-void OctoMapper::Update () {
+void OctoMapper::Update() {
     /* This update does consume time, so do it when necessary:
      * Only update if at least OCTO_MIN_POINTS_TH were added to the
      * PointCloud or if it has not been updated for OCTO_CLEAR_TH_ times
@@ -181,13 +186,13 @@ void OctoMapper::Update () {
     // Not updating during GBAs or while ORB-SLAM not OK
     if (gba_state_) {
         ROS_WARN_THROTTLE(1, "%s: Not updating OctoMap during GBA.", name_of_node_.c_str());
-    } else if (clear_octomap_){
+    } else if (clear_octomap_) {
         // TODO create clearing service?
         octomap_->clear();
         clear_octomap_ = false;
         PublishOctoMap();
         ROS_WARN("%s: Cleared OctoMap.", name_of_node_.c_str());
-    } else if (update_octomap_){
+    } else if (update_octomap_) {
 
         int points_difference = pointcloud_map_points_->size() - previous_cloud_size_;
         points_difference = abs(points_difference);
@@ -206,12 +211,12 @@ void OctoMapper::Update () {
             std::chrono::duration<double> diff = end - begin;
             ROS_DEBUG("%s: OctoMap full update took %f [s].", name_of_node_.c_str(), diff.count());
 
-        // Add only recent points (fast update). Old points won't be updated nor deleted
+            // Add only recent points (fast update). Old points won't be updated nor deleted
         } else if (points_difference > 0) {
             since_last_big_update_++;
             octomap::Pointcloud pointcloud_map_points_reduced;
 
-            int start = (int) previous_cloud_size_/1.1; // heuristic
+            int start = (int) previous_cloud_size_ / 1.1; // heuristic
             for (int i = start; i < pointcloud_map_points_->size(); i++) {
                 pointcloud_map_points_reduced.push_back(pointcloud_map_points_->getPoint(i));
             }
@@ -233,9 +238,9 @@ void OctoMapper::Update () {
 }
 
 
-void OctoMapper::PublishOctoMap () {
+void OctoMapper::PublishOctoMap() {
 
-    if (octomap_publisher_.getNumSubscribers() > 0){
+    if (octomap_publisher_.getNumSubscribers() > 0) {
         octomap_msgs::Octomap msgOctomap;
         msgOctomap.header.frame_id = map_frame_id_param_;
         msgOctomap.header.stamp = ros::Time::now();
@@ -247,16 +252,19 @@ void OctoMapper::PublishOctoMap () {
 }
 
 
-bool OctoMapper::getTfTransformMatrix(Eigen::Affine3d& transform_matrix, const std::string source_frame, const std::string target_frame) {
-
+bool OctoMapper::getTfTransformMatrix(Eigen::Affine3d &transform_matrix, const std::string source_frame,
+                                      const std::string target_frame) {
+    ROS_INFO("Got the function");
     try {
+        ROS_INFO("Trying to find transform from %s to %s", target_frame.c_str(), source_frame.c_str());
         geometry_msgs::TransformStamped transform_to_robot = tfBuffer->lookupTransform(target_frame, source_frame,ros::Time::now(),ros::Duration(0.05));
-
+        ROS_INFO("Get transform");
         transform_matrix = tf2::transformToEigen(transform_to_robot);
+        ROS_INFO("Get transform");
         return true;
     }
     catch (tf2::TransformException &ex) {
-        ROS_WARN("%s",ex.what());
+        ROS_WARN("%s", ex.what());
         return false;
     }
 
